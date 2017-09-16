a89aabe764871c9d0d7bbbc185b2d487294a73ca
Samuel <smejia723@gmail.com>
2017-09-16 18:33:09 -0400

    Add files via upload

--- logs.py
+++ logs.py
@@ -1,8 +1,8 @@
 import psycopg2
-DB_NAME = "news"
+DB_NAME="news"
 
 # what are the most popular three articles of all time?
-query_1 = """select articles.title, count (*) as views
+query_1="""select articles.title, count (*) as views
     from articles join log on articles.slug =
     (regexp_split_to_array(path, E'/article/')) [2]
     where path != '/' group by
@@ -10,7 +10,7 @@ query_1 = """select articles.title, count (*) as views
     articles.title order by views desc limit 3;"""
 
 # Who are the most popular article authors of all time?
-query_2 = """select authors.name,
+query_2="""select authors.name,
     count(articles.author) as views
     from articles, log, authors where log.path =
     concat( E'/article/',articles.slug)
@@ -20,7 +20,7 @@ query_2 = """select authors.name,
 
 
 # On which days did more than 1% of requests lead to errors
-query_3 = """select Date,Total,Error,
+query_3="""select Date,Total,Error,
     (Error::float*100)/total::float as percent
     from (select time::timestamp::date as Date,
     count(status) as Total, sum(case when status =
@@ -37,45 +37,45 @@ def connect():
 def popular_article(query_1):
     """Prints most popular three articles of all time"""
     # Connect to the database
-    db = connect()
+    db=connect()
     # Query command
-    c = db.cursor()
+    c=db.cursor()
     # Objects that runs the queries and results
     c.execute(query_1)
     # Command to fetch the results
-    result = c.fetchall()
+    result=c.fetchall()
     print "\nPopular Articles:\n"
-    for i in range(0, len(result), 1):
-        print "\"" + result[i][0] + "\" - " + str(
-            result[i][1]) + " views"
+    for i in range(0,len(result),1):
+        print "\""+result[i][0]+"\" - "+str(
+            result[i][1])+" views"
     # Close the connection
     db.close()
 
 
 def popular_authors(query_2):
     """Prints most popular article authors of all time"""
-    db = connect()
-    c = db.cursor()
+    db=connect()
+    c=db.cursor()
     c.execute(query_2)
-    result = c.fetchall()
+    result=c.fetchall()
     print "\nPopular Authors:\n"
-    for i in range(0, len(result), 1):
-        print "\"" + result[i][0] + "\" - " + str(
-            result[i][1]) + " views"
+    for i in range(0,len(result), 1):
+        print "\""+result[i][0]+"\" - "+str(
+            result[i][1])+" views"
     db.close()
 
 
 def log_status(query_3):
     """Print days on which more than 1% of requests lead to errors"""
-    db = connect()
-    c = db.cursor()
+    db=connect()
+    c=db.cursor()
     c.execute(query_3)
-    result = c.fetchall()
+    result=c.fetchall()
     print "\nDays with more than 1% of errors:\n"
-    for i in range(0, len(result), 1):
+    for i in range(0,len(result),1):
         print str(
-            result[i][0]) + " - " + str(
-            round(result[i][3], 2)) + "% errors"
+            result[i][0])+" - "+str(
+            round(result[i][3], 2))+"% errors"
     db.close()
 
 
